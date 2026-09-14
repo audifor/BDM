0f51fee4384ae76a5605c7971d5ae0d43dae7a50
audifor <tsamarkanda@gmail.com>
2026-09-14 19:07:56 +0200

    Export World DB competition instance planner

--- src/engine/competition/index.ts
+++ src/engine/competition/index.ts
@@ -1,2 +1,4 @@
 export { getCompetitionSeason, getCompetitionTemporalStatus, isCompetitionActiveOnDate } from './CompetitionLifecycle'
 export { buildNextCompetitionParticipants, getCompetitionTier, getLowerDomesticCompetition, getPromotionRelegationResolution, getUpperDomesticCompetition, resolvePromotionRelegation } from './PromotionRelegation'
+export { instantiateWorldDbCompetitionV1 } from './WorldDbCompetitionInstance'
+export type { InstantiateWorldDbCompetitionV1Input, WorldDbCompetitionInstanceRequirementV1, WorldDbCompetitionInstanceStatusV1, WorldDbCompetitionInstanceV1, WorldDbInstanceFixtureV1 } from './WorldDbCompetitionInstance'
