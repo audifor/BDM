c3285440f7a0d9fa0a4174c7d20748fa4aacbb4b
audifor <tsamarkanda@gmail.com>
2026-09-14 11:39:02 +0200

    Export World DB competition bridge contracts

--- src/domain/competition/index.ts
+++ src/domain/competition/index.ts
@@ -3,3 +3,26 @@ export type { Competition, CreateCompetitionInput } from './Competition'
 export { createCompetitionRules, defaultLeagueCompetitionRules, FIBA_GAME_FORMAT, NBA_GAME_FORMAT, NCAA_MEN_GAME_FORMAT, NCAA_WOMEN_GAME_FORMAT, WNBA_GAME_FORMAT } from './CompetitionRules'
 export type { CompetitionRules, GameFormatRules, StandingsTiebreaker } from './CompetitionRules'
 export type { PromotionRelegationResolution } from './PromotionRelegation'
+export { parseWorldCompetitionFormatDocument } from './WorldCompetitionFormatParser'
+export { WORLD_COMPETITION_FORMAT_SCHEMA_VERSION } from './WorldCompetitionFormatTypes'
+export type {
+  JsonObject,
+  JsonPrimitive,
+  JsonValue,
+  WorldCompetitionCarryover,
+  WorldCompetitionConsequence,
+  WorldCompetitionContest,
+  WorldCompetitionEntrySelection,
+  WorldCompetitionFormatDocument,
+  WorldCompetitionFormatNode,
+  WorldCompetitionFormatVariant,
+  WorldCompetitionHosting,
+  WorldCompetitionOpponentScope,
+  WorldCompetitionPairing,
+  WorldCompetitionProgressionEdge,
+  WorldCompetitionSeeding,
+  WorldCompetitionSource,
+  WorldCompetitionTieResolution,
+} from './WorldCompetitionFormatTypes'
+export { parseWorldCompetitionRuntimeBundle, WORLD_COMPETITION_RUNTIME_BUNDLE_SCHEMA_VERSION } from './WorldCompetitionRuntimeBundle'
+export type { WorldCompetitionRuntimeBundle } from './WorldCompetitionRuntimeBundle'
