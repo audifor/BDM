4152868cdd1af02c0dd688fbc32c813b45fdec5d
audifor <tsamarkanda@gmail.com>
2026-09-14 19:33:43 +0200

    Export World DB competition runtime helpers

--- src/engine/competition/index.ts
+++ src/engine/competition/index.ts
@@ -1,2 +1,7 @@
 export { getCompetitionSeason, getCompetitionTemporalStatus, isCompetitionActiveOnDate } from './CompetitionLifecycle'
 export { buildNextCompetitionParticipants, getCompetitionTier, getLowerDomesticCompetition, getPromotionRelegationResolution, getUpperDomesticCompetition, resolvePromotionRelegation } from './PromotionRelegation'
+export { createWorldDbCompetitionRuntimeV1 } from './WorldDbCompetitionRuntime'
+export { createWorldDbCompetitionRulesV1 } from './WorldDbCompetitionRules'
+export { resolveWorldDbFixtureProgressionV1 } from './WorldDbProgressionResolver'
+export { resolveWorldDbFixturesV1 } from './WorldDbFixtureResolver'
+export { createWorldDbGameFixtureBindingV1 } from './WorldDbGameFixtureBinding'
